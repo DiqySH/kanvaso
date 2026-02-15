a499aa63b7047a703442dc753ff025203a7926de
DiqySH <margolang55@gmail.com>
2026-02-15 21:19:01 +0700

    feat: enhance Examp class to support customizable motion keys

--- src/game/Examp.ts
+++ src/game/Examp.ts
@@ -1,14 +1,31 @@
 import { Input } from "./Input";
 
+type MotionKeys = {
+  up?: string;
+  down?: string;
+  right?: string;
+  left?: string;
+};
+
 export class Examp {
   private x = 0;
   private y = 0;
   private width = 100;
   private height = 100;
   private input: Input;
+  private motionKeys: MotionKeys = {
+    up: "KeyW",
+    down: "KeyS",
+    right: "KeyD",
+    left: "KeyA",
+  };
 
-  constructor(input: Input) {
+  constructor(input: Input, motionKeys?: MotionKeys) {
     this.input = input;
+    this.motionKeys = {
+      ...this.motionKeys,
+      ...motionKeys,
+    };
   }
 
   private isDown(code: string) {
@@ -17,10 +34,10 @@ export class Examp {
 
   update() {
     if (!this.input) return;
-    if (this.isDown("KeyA")) this.x--;
-    if (this.isDown("KeyW")) this.y--;
-    if (this.isDown("KeyS")) this.y++;
-    if (this.isDown("KeyD")) this.x++;
+    if (this.isDown(this.motionKeys.left || "KeyA")) this.x--;
+    if (this.isDown(this.motionKeys.up || "KeyW")) this.y--;
+    if (this.isDown(this.motionKeys.down || "KeyS")) this.y++;
+    if (this.isDown(this.motionKeys.right || "KeyD")) this.x++;
   }
 
   render(ctx: CanvasRenderingContext2D) {
